a438a5f57434a059e7bc722525f6972cb2aacd16
Sergiu Puscas <srg.pscs@gmail.com>
2026-04-17 10:09:35 +0200

    Adjust fitness badge number alignment

--- frontend/src/components/icons/energy-cost-icon.tsx
+++ frontend/src/components/icons/energy-cost-icon.tsx
@@ -1,6 +1,6 @@
 import { numericalStr } from "@/utils/card-utils";
 import { cx } from "@/utils/cx";
-import { AspectIcon, ASPECT_DISPLAY_NAMES } from "./aspect-icon";
+import { AspectIcon } from "./aspect-icon";
 import css from "./energy-cost-icon.module.css";
 
 type Props = {
@@ -10,19 +10,14 @@ type Props = {
 };
 
 export function EnergyCostIcon({ aspect, className, cost }: Props) {
-  const displayName = aspect
-    ? (ASPECT_DISPLAY_NAMES[aspect as keyof typeof ASPECT_DISPLAY_NAMES] ?? aspect)
-    : null;
-
   return (
     <div className={css["container"]}>
-      <div className={cx(css["badge"], className)}>
+      <div className={cx(css["badge"], className)} data-aspect={aspect ?? undefined}>
         <AspectIcon aspect={aspect} className={css["badge-svg"]} />
         {cost != null && (
           <span className={css["badge-number"]}>{numericalStr(cost)}</span>
         )}
       </div>
-      {displayName && <span className={css["badge-type"]}>{displayName}</span>}
     </div>
   );
 }
