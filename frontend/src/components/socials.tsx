fb09446ab544797409dab31de028f3b709685047
Sergiu Puscas <srg.pscs@gmail.com>
2026-04-19 09:03:57 +0200

    Update about page credits and links

--- frontend/src/components/socials.tsx
+++ frontend/src/components/socials.tsx
@@ -1,7 +1,4 @@
-import { BoardGameGeekIcon } from "./icons/boardgamegeek-icon";
-import { DiscordIcon } from "./icons/discord-icon";
 import { GithubIcon } from "./icons/github-icon";
-import { PatreonIcon } from "./icons/patreon-icon";
 import { Button } from "./ui/button";
 
 export function Socials(props: { className?: string }) {
@@ -9,7 +6,7 @@ export function Socials(props: { className?: string }) {
     <div className={props.className}>
       <Button
         as="a"
-        href="https://github.com/arkham-build/arkham.build"
+        href="https://github.com/harababurel/earthborne.build"
         target="_blank"
         rel="noreferrer"
         variant="bare"
@@ -17,39 +14,6 @@ export function Socials(props: { className?: string }) {
         size="lg"
       >
         <GithubIcon />
-      </Button>
-      <Button
-        as="a"
-        href="https://www.patreon.com/c/arkhambuild"
-        target="_blank"
-        rel="noreferrer"
-        variant="bare"
-        iconOnly
-        size="lg"
-      >
-        <PatreonIcon />
-      </Button>
-      <Button
-        as="a"
-        href="https://boardgamegeek.com/thread/3497091/arkhambuild-a-web-deck-builder"
-        target="_blank"
-        rel="noreferrer"
-        variant="bare"
-        iconOnly
-        size="lg"
-      >
-        <BoardGameGeekIcon />
-      </Button>
-      <Button
-        as="a"
-        href="https://discord.gg/tq96ZJrXtY"
-        target="_blank"
-        rel="noreferrer"
-        variant="bare"
-        iconOnly
-        size="lg"
-      >
-        <DiscordIcon />
       </Button>
     </div>
   );
--- frontend/src/pages/about/about.tsx
+++ frontend/src/pages/about/about.tsx
@@ -11,8 +11,6 @@ function About() {
   const goBack = useGoBack();
   const { t } = useTranslation();
 
-  const date = new Date().getUTCFullYear();
-
   return (
     <AppLayout title={t("about.title")}>
       <div className={cx("longform", css["about"])}>
@@ -24,12 +22,15 @@ function About() {
         <Trans
           i18nKey="about.description"
           t={t}
-          values={{ date }}
           components={{
-            ffg: (
+            arkham_build_url: (
+              // biome-ignore lint/a11y/useAnchorContent: not relevant here.
+              <a href="https://arkham.build" rel="noreferrer" target="_blank" />
+            ),
+            eb: (
               // biome-ignore lint/a11y/useAnchorContent: not relevant here.
               <a
-                href="https://www.fantasyflightgames.com/en/products/arkham-horror-the-card-game/"
+                href="https://earthbornegames.com/earthborne-rangers/"
                 rel="noreferrer"
                 target="_blank"
               />
@@ -38,10 +39,10 @@ function About() {
               // biome-ignore lint/a11y/useAnchorContent: not relevant here.
               <a href="https://spoettel.dev" rel="noreferrer" target="_blank" />
             ),
-            contrib_url: (
+            github_issues_url: (
               // biome-ignore lint/a11y/useAnchorContent: not relevant here.
               <a
-                href="https://github.com/fspoettel/arkham.build/graphs/contributors"
+                href="https://github.com/harababurel/earthborne.build/issues"
                 rel="noreferrer"
                 target="_blank"
               />
@@ -49,91 +50,31 @@ function About() {
             github_url: (
               // biome-ignore lint/a11y/useAnchorContent: not relevant here.
               <a
-                href="https://github.com/fspoettel/arkham.build"
+                href="https://github.com/harababurel/earthborne.build"
                 rel="noreferrer"
                 target="_blank"
               />
             ),
           }}
         />
-        <h2>🌟 {t("about.hall_of_fame.title")} 🌟</h2>
-        <ul>
-          <li>
-            <strong>@zzorba:</strong> {t("about.hall_of_fame.zzorba")} 🙇‍♂️
-          </li>
-          <li>
-            <strong>@Chr1Z</strong>, <strong>@Dangaroo</strong>,{" "}
-            <strong>@5argon</strong>: {t("about.hall_of_fame.testing")}
-          </li>
-          <li>
-            <strong>@TartanLlama</strong>, <strong>@MickeyTheQ</strong>,{" "}
-            <strong>@christoph-frick</strong>, <strong>@blu</strong>,{" "}
-            <strong>@5argon</strong>, <strong>@Marymatician</strong>:{" "}
-            {t("about.hall_of_fame.code")}
-          </li>
-          <li>
-            <strong>@kamalisk / ArkhamDB: </strong>{" "}
-            {t("about.hall_of_fame.arkham_db")}
-          </li>
-          <li>
-            <strong>@morvael</strong>:{" "}
-            {t("about.hall_of_fame.customization_api")}
-          </li>
-          <li>
-            <strong>@HatfulBob</strong>, <strong>@coldtoes</strong>:{" "}
-            {t("about.hall_of_fame.card_design")}
-          </li>
-          <li>
-            <strong>@Adran06</strong>, <strong>@coldtoes</strong>,{" "}
-            <strong>@VinnyB</strong>: {t("about.hall_of_fame.qa")}
-          </li>
-          <li>
-            <strong>@dlloyd</strong>: {t("about.hall_of_fame.metadata")}
-          </li>
-        </ul>
-        <p>{t("about.hall_of_fame.translation.title")}:</p>
-        <ul>
-          <li>
-            {t("about.hall_of_fame.translation.ko")}:{" "}
-            <strong>푸른이(@derornos)</strong>
-          </li>
-          <li>
-            {t("about.hall_of_fame.translation.pl")}: <strong>@javnik36</strong>
-          </li>
-          <li>
-            {t("about.hall_of_fame.translation.ru")}:{" "}
-            <strong>@Evgeny727</strong>
-          </li>
-          <li>
-            {t("about.hall_of_fame.translation.fr")}: <strong>@Zaratan</strong>
-          </li>
-          <li>
-            {t("about.hall_of_fame.translation.es")}:{" "}
-            <strong>@AdrianMeizoso</strong>
-          </li>
-          <li>
-            {t("about.hall_of_fame.translation.zh")}:{" "}
-            <strong>泡菜大王(@Ruikoto)</strong>
-          </li>
-        </ul>
         <h2>{t("about.image_credits.title")}</h2>
         <ul>
           <li>
-            <strong>{t("about.image_credits.card_icons")}:</strong> Fantasy
-            Flight Games
+            <strong>{t("about.image_credits.card_icons")}:</strong>{" "}
+            {t("about.image_credits.earthborne_games")}
           </li>
           <li>
-            <strong>{t("about.image_credits.logo")}:</strong> Dangaroo
+            <strong>{t("about.image_credits.logo")}:</strong>{" "}
+            {t("about.image_credits.earthborne_games")}
           </li>
           <li>
-            <strong>{t("about.image_credits.arkham_cards_icons")}:</strong>{" "}
-            Eugene Sarnetsky
+            {/* TODO: Replace this art with something more appropriate for earthborne.build. */}
+            <strong>{t("about.image_credits.404")}:</strong>{" "}
+            {t("about.image_credits.404_credit")}
           </li>
           <li>
-            <strong>{t("about.image_credits.404")}:</strong> FFG & 5argon
-          </li>
-          <li>
-            <strong>{t("about.image_credits.other")}</strong>: lucide.dev
+            <strong>{t("about.image_credits.other")}</strong>:{" "}
+            {t("about.image_credits.other_credit")}
           </li>
         </ul>
       </div>
